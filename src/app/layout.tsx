78a8c829be0671505be9695c312053d94eb6bfea
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-27 12:32:09 +0800

    init nextjs with react-email

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Claysmas",
+  description: "Clanta is coming to town",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
