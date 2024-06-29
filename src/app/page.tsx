b4cb6c37ac47372f0fb71290944a464c030b86f7
JoeMcCleery <mccleery.joseph@gmail.com>
2024-06-29 10:47:44 +0800

    dim background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,12 @@ import BackgroundImage from "@/assets/background.jpg";
 export default function Home() {
   return (
     <main
-      className="grid grid-cols-[1fr] min-h-screen p-2 justify-center items-center"
+      className="relative grid grid-cols-[1fr] min-h-screen p-2 justify-center items-center"
       style={{
         background: `repeat center/500px url(${BackgroundImage.src})`,
       }}
     >
+      <div className="absolute inset-0 bg-black/50"></div>
       <div className="mx-auto max-w-md p-6 bg-gray-800/80 rounded backdrop-blur-lg text-white">
         <h1 className="text-5xl font-bold text-center mb-6">Merry Claysmas!</h1>
         <ContactForm />
